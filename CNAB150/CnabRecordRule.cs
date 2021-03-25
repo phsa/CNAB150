b4bd2670ce8f36f4d31cfb55c9202f3c21acbe53
phsa <phsaraujo54@gmail.com>
2021-03-24 21:54:08 -0300

    #002 - Simple CNAB rule definition

---
+++ CNAB150/CnabRecordRule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CNAB150
+{
+    class CnabRecordRule
+    {
+        public int Length { get; set; }
+        public string AllowedCharacters { get; set; }
+        public char FillingChar { get; set; }
+        public string Description { get; set; }
+        public bool FillAtEnd { get; set; }
+
+        private Regex Expression
+        {
+            get
+            {
+                string pattern = $"^[{FillingChar}{AllowedCharacters}]{{{Length},{Length}}}$";
+                return new Regex(pattern);
+            }
+        }
+        private Func<string, int, char, string> FillingMethod
+        {
+            get
+            {
+                if (FillAtEnd)
+                {
+                    return StringUtils.FillAtEnd;
+                }
+                else
+                {
+                    return StringUtils.FillAtStart;
+                }
+            }
+        }
+
+        public string Apply(string str)
+        {
+            return str.Fit(Length, FillingChar, FillingMethod); //ADD ALLOWED CHARACTERS CHECK [REPLACE OR REMOVE METHODS]
+        }
+
+        public bool Check(string str)
+        {
+            return Expression.IsMatch(str);
+        }
+    }
+}
